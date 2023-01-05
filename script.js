abb67651c0123d3fa55d32fe6a68212b9b88397d
Tijuana22 <tijuanahunt39@gmail.com>
2023-01-04 20:22:07 -0500

    updating repo

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let quotes = [
   `It don't matter if you win by an inch or a mile. Winning's winning.`
 ];
 
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function(evt) {
   // Random quote of the day generator
   const randomQuote = function() {
     document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
@@ -18,39 +18,69 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   // Part 1
  document.querySelector("#main-title")
- .textContent ="I'm DOM Toretto, Welcome to my page.";
+ .innerHTML ="Welcome to DOM Toretto's homepage!";
  
   // Part 2
   document.querySelector("body")
-  .style.backgroundColor = "blue";
+  .style.backgroundColor = "#A0522D";
   
   // Part 3
-  let favoriteThings =document.querySelectorAll("#favorite-things > li")
+  document.querySelectorAll("#favorite-things > li")
   favoriteThings =document.querySelector("li:last-child");
   favoriteThings.remove()
 
   // Part 4
-  let specialTitle =document.querySelectorAll(".special-title")
-  specialTitle.forEach((item) =>{
+  document.querySelectorAll(".special-title")
+  .forEach((item) =>{
     item.style.fontSize = "2rem"
 
   })
   // Part 5
-  let pastRaces =document.querySelectorAll("#past-races > li")
-  pastRaces =document.querySelectorAll;
+  document.querySelectorAll("#past-races > li")
+  .forEach((item) =>{
+    if (item.textContent ===  "Chicago"){
+      document.querySelector("#past-races").removeChild(item)
+
+    }
+  })
   
 
 
   // Part 6
-
+  let li = document.createElement("li")
+  li.textContent = "Baja Mexico"
+  document.querySelector('#past-races')
+  .appendChild(li)
 
   // Part 7
+  let blogPost =document.createElement("div")
+  blogPost.classList.add("blog-post")
+  let heading =document.createElement("h2")
+  heading.innerHTML = "Baja Mexico"
+  let pEl =document.createElement("p")
+  pEl.innerHTML = "I escaped L.A to Mexico, where I drove my red Chevelle"
+
+  document.querySelector(".main").appendChild(blogPost)
+  blogPost.appendChild(heading)
+  blogPost.appendChild(pEl)
+
 
 
   // Part 8
-
-
+  document.querySelector("#quote-title").addEventListener("click", (evt) => {
+  randomQuote()
+  }) 
   // Part 9
+document.querySelector(".blog-post").forEach((item) => {
+  item.addEventListener("mouseout",(evt) =>{
+    evt.currentTarget.classList.toggle('purple')
+  })
+
+  item.addEventListener("mouseenter", (evt) => {
+    evt.stopPropagation()
+    evt.currentTarget.classList.toggle('red')
+  })
+})
 
 
 
